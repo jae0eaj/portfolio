d60fd69c89ec4acf33724f653c4e3516d6abec5d
jae0eaj <86229084+jae0eaj@users.noreply.github.com>
2024-12-02 21:17:59 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+
+    var timeString = `${hours}:${minutes}:${seconds}(KR)`;
+    document.getElementById('clock').textContent = timeString;
+}
+
+// 매 초마다 시계 업데이트
+setInterval(updateClock, 1000);
+
+// 페이지 로드 시에도 시계 업데이트
+updateClock();
+
+
